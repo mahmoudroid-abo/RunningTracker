1744155e4e98578bf5fd0d4232c834cfb8ca8172
maboe <maboelenen555@gmail.com>
2021-04-13 23:21:35 +0200

    add viewModels&UI

--- app/src/main/java/com/mahmoudroid/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/com/mahmoudroid/runningtracker/ui/MainActivity.kt
@@ -1,8 +1,8 @@
-package com.mahmoudroid.runningtracker
+package com.mahmoudroid.runningtracker.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import com.mahmoudroid.runningtracker.R
 import com.mahmoudroid.runningtracker.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
---
+++ app/src/main/java/com/mahmoudroid/runningtracker/ui/fragments/SetUpFragment.kt
@@ -0,0 +1,7 @@
+package com.mahmoudroid.runningtracker.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.mahmoudroid.runningtracker.R
+
+class SetUpFragment : Fragment(R.layout.fragment_setup) {
+}
