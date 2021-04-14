d33c13a558d84e4a60a8151d8ce9cac6cbe03424
maboe <maboelenen555@gmail.com>
2021-04-14 03:11:56 +0200

    add navigation

--- app/src/main/java/com/mahmoudroid/runningtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/mahmoudroid/runningtracker/ui/fragments/RunFragment.kt
@@ -1,13 +1,27 @@
 package com.mahmoudroid.runningtracker.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.mahmoudroid.runningtracker.R
 import com.mahmoudroid.runningtracker.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
+import kotlinx.android.synthetic.main.fragment_setup.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+
+    }
 }
